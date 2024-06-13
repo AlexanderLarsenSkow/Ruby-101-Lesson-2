96b3891f8d720a7a85e0aa46fb1c131fd69c1bba
Alex Skow <askow2015@gmail.com>
2024-06-13 18:53:32 +0000

    Finished user_choice Method

--- rps/rps.rb
+++ rps/rps.rb
@@ -25,14 +25,40 @@ def get_username
 		break unless username.empty?
 		prompt(MESSAGES['username_error'])
 	end 
-	prompt("#{MESSAGES['username_1']} #{username}. #{MESSAGES['username_2']}")
+	prompt("#{MESSAGES['username_1']} #{username}, #{MESSAGES['username_2']}")
 	username 
+end 
+
+# Responding to the User's Choice
+
+def responses(user_choice) 
+	case user_choice
+		when 1 then prompt(MESSAGES['chose_rock'])
+		when 2 then prompt(MESSAGES['chose_paper'])
+		else 
+			prompt(MESSAGES['chose_scissors'])
+	end 
+	prompt(MESSAGES['commence_battle'])
+end 
+
+def get_user_choice
+	user_choice = 0
+	loop do 
+		prompt(MESSAGES['user_choice'])
+		user_choice = gets.chomp.to_i
+		
+		break if input_validation(user_choice)
+		prompt(MESSAGES['user_choice_error'])
+	end 
+	responses(user_choice)
+	user_choice
 end 
 
 # Main Game 
 
 def main_game
 	username = get_username
+	user_choice = get_user_choice 
 end 	
 
 main_game
