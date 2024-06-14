dc9adb45328bfc4166cdf9d1aa8e09fe1d6d5c2f
Alex Skow <askow2015@gmail.com>
2024-06-14 02:27:44 +0000

    Added Points Feature for RPS

--- rps/rps.rb
+++ rps/rps.rb
@@ -31,7 +31,7 @@ end
 
 # Responding to the User's Choice
 
-def responses(user_choice) 
+def display(user_choice) 
 	case user_choice
 		when 1 then prompt(MESSAGES['chose_rock'])
 		when 2 then prompt(MESSAGES['chose_paper'])
@@ -48,7 +48,7 @@ def get_user_choice
 		break if input_validation(user_choice)
 		prompt(MESSAGES['user_choice_error'])
 	end 
-	responses(user_choice)
+	display(user_choice)
 	user_choice
 end 
 
@@ -61,7 +61,7 @@ def get_computer_choice
 	case computer_choice
 		when 1 then prompt(MESSAGES['comp_rock']) 
 		when 2 then prompt(MESSAGES['comp_paper'])
-		when 3 then prompt(MESSAGES['comp_ascissors'])
+		when 3 then prompt(MESSAGES['comp_scissors'])
 	end 
 	computer_choice
 end 
@@ -76,9 +76,7 @@ def increment_for_user(user_points)
 	user_points += 1 
 end 
 
-# Defining the Game's Logic 
-
-
+# Defining the Game's Logic with Helper Methods
 
 def tie_game(user_choice, computer_choice) 
 	if user_choice == computer_choice
@@ -86,64 +84,54 @@ def tie_game(user_choice, computer_choice)
 	end 
 end 
 
-def user_win(user_choice, computer_choice)
+def user_win(user_choice, computer_choice, user_won)
+	
 	if user_choice == 1 && computer_choice == 3
 		prompt(MESSAGES['user_won_rock'])
+		user_won = true
 		
 	elsif user_choice == 2 && computer_choice == 1
 		prompt(MESSAGES['user_won_paper'])
+		user_won = true
 		
 	elsif user_choice == 3 && computer_choice == 2
-		prompt(MESSAGES['user_won_scissors'])	
-	end 
+		prompt(MESSAGES['user_won_scissors'])
+		user_won = true
+	end
 end 
 
-def comp_win(user_choice, computer_choice) 
+def comp_win(user_choice, computer_choice, comp_won)
+	
 	if user_choice == 1 && computer_choice == 2
 		prompt(MESSAGES['comp_won_paper'])
+		comp_won = true
 		
 	elsif user_choice == 2 && computer_choice == 3
 		prompt(MESSAGES['comp_won_scissors'])
+		comp_won = true
 
 	elsif user_choice == 3 && computer_choice == 1
 		prompt(MESSAGES['comp_won_rock'])
+		comp_won = true
 	end 
 end 
+
+# Primmary Logic Method
 
 def game_logic(user_choice, computer_choice) 
-	user_points = 0
-	comp_points = 0
+	user_won = false
+	comp_won = false
 	
-	tie_game(user_choice, computer_choice)
-	user_win(user_choice, computer_choice)
-	comp_win(user_choice, computer_choice)
+	tie_point = tie_game(user_choice, computer_choice)
+	user_won = user_win(user_choice, computer_choice, user_won)
+	comp_won = comp_win(user_choice, computer_choice, comp_won)
 	
+	if user_won == true 
+		true 
+	elsif comp_won == true
+		false 
+	end
 end 
-=begin
-def game_logic(user_choice, computer_choice)
-	if user_choice == computer_choice 
-		prompt(MESSAGES['tie'])
-		
-	elsif user_choice == 1 && computer_choice == 2
-		prompt(MESSAGES['comp_won_paper'])
-
-	elsif user_choice == 1 && computer_choice == 3
-		prompt(MESSAGES['user_won_rock'])
-
-	elsif user_choice == 2 && computer_choice == 1
-		prompt(MESSAGES['user_won_paper'])
-
-	elsif user_choice == 2 && computer_choice == 3
-		prompt(MESSAGES['comp_won_scissors'])
-
-	elsif user_choice == 3 && computer_choice == 1
-		prompt(MESSAGES['comp_won_rock'])
-	else 
-		prompt(MESSAGES['user_won_scissors'])
-	end 
-	prompt("Score: user: #{user_points} Rockbot: #{comp_points}")
-end 
-=end 
 
 # Main Game 
 
@@ -157,7 +145,14 @@ def main_game
 		computer_choice = get_computer_choice
 		result = game_logic(user_choice, computer_choice)
 		
-	prompt("User: #{user_points} / Rockbot: #{comp_points}")
+		if result == true 
+			user_points += 1
+		elsif result == false 
+			comp_points += 1
+		end 
+		prompt("User: #{user_points} / Rockbot: #{comp_points}")
+		
+		break if user_points == 10 || break if comp_points == 10
 	end 
 end 	
 
