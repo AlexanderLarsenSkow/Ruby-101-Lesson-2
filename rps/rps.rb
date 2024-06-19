75219e66e56ff48ec134d57990a3c892a20941d1
Alex Skow <askow2015@gmail.com>
2024-06-19 21:31:38 +0000

    Cleaned up Timing on Sleep / Clear

--- rps/rps.rb
+++ rps/rps.rb
@@ -113,20 +113,21 @@ end
 # Getting the User's Choice
 
 def get_user_choice(choices, choices_abr)
-  sleep_for 3.5
+  sleep_for 2
   clear
   
   user_choice = ''
   loop do
     prompt(MESSAGES['user_choice'])
     user_choice = gets.chomp.capitalize
+    clear
 
     break if input_validation(user_choice, choices, choices_abr)
     prompt(MESSAGES['user_choice_error'])
   end
   
   display_user(user_choice)
-  sleep_for 0.6
+  sleep_for 0.65
   user_choice
 end
 
@@ -149,7 +150,7 @@ def get_computer_choice
   computer_choice = choices_array.sample
 
   display_comp(computer_choice)
-  sleep_for 0.6
+  sleep_for 0.65
   computer_choice
 end
 
@@ -187,11 +188,10 @@ end
 # Displaying values based on return value Class in find_winner
 
 def display_results(results_method)
-    sleep_for 0.6
     
     if results_method.is_a?(Array)
       prompt(results_method[0])
-      sleep_for 0.6 
+      sleep_for 0.65
       prompt(results_method[1])
     else 
       prompt(results_method)
@@ -201,7 +201,15 @@ end
 # Display Win or Loss to the User
 
 def display_win_loss(username, user_points, comp_points)
-  sleep_for 1
+  sleep_for 0.65
+  prompt(format("You: %s / Rockbot: %s", user_points, comp_points))
+  
+  sleep_for 0.65
+  
+  if user_points == 3 || comp_points == 3
+    clear
+  end 
+  
   prompt("#{username}, #{MESSAGES['win']}") if user_points == 3
   prompt("#{username}, #{MESSAGES['lose']}") if comp_points == 3
 end
@@ -213,7 +221,6 @@ def new_game?(_user_points, _comp_points)
   gets.chomp.upcase
 end
 
-clear
 # Let's Play
 
 username = get_username
@@ -237,7 +244,6 @@ loop do
     comp_points += 1
   end
 
-  prompt(format("You: %s / Rockbot: %s", user_points, comp_points))
   display_win_loss(username, user_points, comp_points)
 
   if user_points == 3 || comp_points == 3
@@ -249,4 +255,5 @@ loop do
   end
 end
 
+clear
 prompt("#{MESSAGES['thanks']} #{username}!")
